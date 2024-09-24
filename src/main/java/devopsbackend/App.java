86651ecb2ac2d0544e938a806c24b04b191ee2a0
piexie3 <emmanuelkbett3@gmail.com>
2024-09-25 01:02:56 +0300

    initial commit

---
+++ src/main/java/devopsbackend/App.java
@@ -0,0 +1,13 @@
+package devopsbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
