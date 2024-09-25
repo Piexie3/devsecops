7e208093b22f7d2a8bfa1a39f019066da98fc54c
piexie3 <emmanuelkbett3@gmail.com>
2024-09-25 04:52:40 +0300

    authentication api

---
+++ src/main/java/devopsbackend/repository/UsersRepo.java
@@ -0,0 +1,11 @@
+package devopsbackend.repository;
+
+import devopsbackend.entity.OurUsers;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepo extends JpaRepository<OurUsers, Integer> {
+
+    Optional<OurUsers> findByEmail(String email);
+}
